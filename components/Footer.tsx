f4293b11b8d32ac2bc0f17104e6600817ac35991
vladyslav.novalenko <vladyslav.novalenko@infotech.gov.ua>
2023-01-12 16:10:00 +0200

    refactor

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,36 +2,26 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from '../styles/Footer.module.scss'
 
+const contactsInfo =[
+  {id:1, path:'tel: +380967181951', text:'+380967181951', src: '/Telegram.png'},
+  {id:2, path:'tel: +380967181951', text:'+380967181951', src: '/Viber.png'},
+  {id:3, path:'https://uk-ua.facebook.com/', text:'Facebook', src: '/Facebook.png'},
+  {id:4, path:'https://www.instagram.com/', text:'Instagram', src: '/Instagram.png'},
+  {id:5, path:'https://www.ukr.net/ua/', text:'Email', src: '/Email.png'},
+  {id:6, path:'https://twitter.com/?lang=uk', text:'Twitter', src: '/Twitter.png'},
+];
+
 const Footer = () => (
   <footer className={styles.footer}>
-    <Link href='/'>
+    {/* <Link href='/'>
       <Image src='/logo.png' width={160} height={80} alt='Логотип'/>
-    </Link>
+    </Link> */}
     <div className={styles.contacts}>
-    <Link href='tel: +380967181951'>
-      <Image src='/Telegram.png' width={30} height={30} alt='Логотип'/>
-      <span>+380967181951</span>
-    </Link>
-    <Link href='tel: +380967181951'>
-      <Image src='/Viber.png' width={30} height={30} alt='Логотип'/>
-      <span>+380967181951</span>
-    </Link>
-    <Link href='https://uk-ua.facebook.com/'>
-      <Image src='/Facebook.png' width={30} height={30} alt='Логотип'/> 
-      <span>Facebook</span>
-    </Link>
-    <Link href='https://www.instagram.com/'>
-      <Image src='/Instagram.png' width={30} height={30} alt='Логотип'/>
-      <span>Instagram</span>
-    </Link>
-    <Link href=''>
-      <Image src='/Email.png' width={30} height={30} alt='Логотип'/>
-      <span>Email</span>
-    </Link>
-    <Link href='https://twitter.com/?lang=uk'>
-      <Image src='/Twitter.png' width={30} height={30} alt='Логотип'/>
-      <span>Twitter</span>
-    </Link>      
+    {contactsInfo.map(({id, path, text, src})=>
+    <Link className={styles.link} key={id} href={path}>
+      <Image src={src} width={30} height={30} alt='Логотип'/>
+      <span className={styles.text}>{text}</span>
+    </Link>)}
     </div>    
   </footer>
 );
